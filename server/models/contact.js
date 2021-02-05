2309cec34d8fac68de98b98c94f8108cf515cb69
rxjas <christopherrojas35@yahoo.com>
2021-02-05 10:22:13 -0700

    created model for the contact to come in, populated the index to easily call upon

---
+++ server/models/contact.js
@@ -0,0 +1,33 @@
+//require mongoose
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+    
+    name: {
+        type: String, 
+        required: true, 
+        trim: true 
+    },
+
+    email: {
+        type: String, 
+        required: true, 
+        trim: true
+    },
+
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true
+    }
+});
+
+const Contact = mongoose.model('Contact', ContactSchema);
+
+module.exports = Contact;
