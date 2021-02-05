bdfac1b4af1613145390fd7f7cb37ee5c175eccb
rxjas <christopherrojas35@yahoo.com>
2021-02-05 09:42:30 -0700

    Created the bare bones server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+// connect to routes const routes = require()
+const path = require('path');
+const app = express();
+const logger = require('morgan');
+//require the DB connection
+
+//Port default 3000 otherwise use the one assigned by heroku
+const PORT = process.env.PORT || 3001;
+
+//Middleware for parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger);
+// app.use(routes);
+
+// if (process.env.NODE_ENV === "production") {
+//    app.use(express.static("client/build"));
+//   app.get('*', (req, res) => {
+//      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+//    });
+// }
+
+//Letting us know if the server is listening emoji may cause issues
+app.listen(PORT, () => {
+    console.log('😤👌 App Running on PORT: '+ PORT)
+});
