cbc93d2050ac6ef636d4df2dafbe6c762bbf2de8
rxjas <christopherrojas35@yahoo.com>
2021-04-02 22:44:44 -0600

    Able to create a contact, fixed morgan logger

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
-// connect to routes const routes = require()
+const routes = require('./routes')
 const path = require('path');
 const app = express();
 const logger = require('morgan');
 require ('./config/db.js')();
 
 //Port default 3000 otherwise use the one assigned by heroku
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 //Middleware for parsing data and use morgan logger
+app.use(logger('combined'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logger);
-// app.use(routes);
+app.use(routes);
 
 // if (process.env.NODE_ENV === "production") {
 //    app.use(express.static("client/build"));
